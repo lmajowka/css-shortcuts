bd819ed3e34f6d431d3c7a634342fd390b72576d
lmajowka <leo.majowka@gmail.com>
2017-11-06 18:11:46 +0200

    add spec for reading multiple files	and add callback for #buildCss

--- spec/css_generator_spec.js
+++ spec/css_generator_spec.js
@@ -2,20 +2,47 @@ let CSSGenerator = require('../lib/css_generator');
 
 describe('CSSGenerator', () => {
 
-  describe('matchClasses', () => {
+  describe('#matchClasses', () => {
 
-    beforeEach(function(done){
-    	CSSGenerator.readFile('./examples/example.html', done);
+    describe('with 	one file', () => {
+
+      beforeEach(function(done){
+      	CSSGenerator.startReading(1, done);
+      	CSSGenerator.readFile('./examples/example.html');
+      });
+
+      let exampleClasses = [ 'm40', 'ml10', 'ml20', 'mr15', 'mr25', 'mt20', 'mt4', 'mb6', 'mb13', 'p4', 'pl10', 'pb30', 'fs30', 'fs22', 'fs34', 'fs14' ];
+
+      it('stored the right matched classes', (done) => {
+
+        expect(CSSGenerator.matchedClasses).toEqual(exampleClasses);
+        done();
+      
+      });
+
     });
+  
 
-    let exampleClasses = [ 'm40', 'ml10', 'ml20', 'mr15', 'mr25', 'mt20', 'mt4', 'mb6', 'mb13', 'p4', 'pl10', 'pb30', 'fs30', 'fs22', 'fs34', 'fs14' ];
+    describe('with multiple files', () => {
 
-    it('stored the right matched classes', (done) => {
+      beforeEach(function(done){
+      	CSSGenerator.reset();	
+      	CSSGenerator.startReading(2	, done);
+      	CSSGenerator.readFile('./examples/example3.html');
+      	CSSGenerator.readFile('./examples/example4.html');
+      });
 
-      expect(CSSGenerator.matchedClasses).toEqual(exampleClasses);
-      done();
+      let exampleClasses = [ 'ml2', 'ml4', 'mr1', 'mr3', 'mr5', 'ml3', 'ml5', 'mr2', 'mr4', 'mr6' ]	
+
+      it('stored the right matched classes', (done) => {
+
+        expect(CSSGenerator.matchedClasses).toEqual(exampleClasses);
+        done();
+      
+      });
       
     });
+
   });
 
 });
